a13b93f8fdacd09bc8ff576ff025cc891480b4ee
Zackary Harnett <ZackaryHarnett@gmail.com>
2021-04-18 10:05:08 -0400

    Adding empty Game and BowlingGameTest classes

---
+++ src/main/java/bowling/kata/Game.java
@@ -0,0 +1,6 @@
+package bowling.kata;
+
+public class Game {
+
+	
+}
---
+++ src/test/java/bowling/kata/BowlingGameTest.java
@@ -0,0 +1,11 @@
+package bowling.kata;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class BowlingGameTest extends TestCase{
+
+
+}
