1962ebb3106ed651dca96b11f74275cca714e3d1
Zackary Harnett <ZackaryHarnett@gmail.com>
2021-04-18 10:17:44 -0400

    First test working

--- src/main/java/bowling/kata/Game.java
+++ src/main/java/bowling/kata/Game.java
@@ -1,6 +1,12 @@
 package bowling.kata;
 
 public class Game {
-
 	
+	public void roll(int pins) {
+		
+	}
+	
+	public void score() {
+		
+	}
 }
--- src/test/java/bowling/kata/BowlingGameTest.java
+++ src/test/java/bowling/kata/BowlingGameTest.java
@@ -1,11 +1,13 @@
 package bowling.kata;
 
-
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 public class BowlingGameTest extends TestCase{
 
-
+	public void testGutterGame() throws Exception {
+		Game g = new Game();
+	}
+	
 }
