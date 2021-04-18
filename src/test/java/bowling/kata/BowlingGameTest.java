daf416d01adc0b030e5eca1ff1012838fa98c2b2
Zackary Harnett <ZackaryHarnett@gmail.com>
2021-04-18 10:25:26 -0400

    Added functionality to make testAllOnes pass

--- src/test/java/bowling/kata/BowlingGameTest.java
+++ src/test/java/bowling/kata/BowlingGameTest.java
@@ -17,7 +17,7 @@ public class BowlingGameTest extends TestCase{
 	public void testAllOnes() throws Exception {
 		Game g = new Game();
 		for(int i = 0; i < 20; i++) {
-			g.roll(0);
+			g.roll(1);
 		}
 		assertEquals(20, g.score());
 	}
