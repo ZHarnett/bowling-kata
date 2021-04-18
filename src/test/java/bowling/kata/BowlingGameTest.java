f45f4953962e8be0ede1f54c99caa6e97260d247
Zackary Harnett <ZackaryHarnett@gmail.com>
2021-04-18 10:41:46 -0400

    Added testOneSpare and logic to Game.java to make it pass
    Added rollMany and rollSpare to BowlingGameTest as well

--- src/test/java/bowling/kata/BowlingGameTest.java
+++ src/test/java/bowling/kata/BowlingGameTest.java
@@ -5,20 +5,37 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class BowlingGameTest extends TestCase{
-
-	public void testGutterGame() throws Exception {
-		Game g = new Game();
-		for(int i = 0; i < 20; i++) {
-			g.roll(0);
+	private Game g;
+	
+	protected void setUp() throws Exception {
+		g = new Game();
+	}
+	
+	private void rollMany(int n, int pins) {
+		for(int i = 0; i < n; i++) {
+			g.roll(pins);
 		}
+	}
+	
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+	
+	public void testGutterGame() throws Exception {
+		rollMany(20, 0);
 		assertEquals(0, g.score());
 	}
 	
 	public void testAllOnes() throws Exception {
-		Game g = new Game();
-		for(int i = 0; i < 20; i++) {
-			g.roll(1);
-		}
+		rollMany(20, 1);
 		assertEquals(20, g.score());
+	}
+	
+	public void testOneSpare() throws Exception {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
 	}
 }
