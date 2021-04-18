2653e86d54682687e133b854d5f921ac0375a793
Zackary Harnett <ZackaryHarnett@gmail.com>
2021-04-18 10:23:35 -0400

    testAllOnes added and fails

--- src/test/java/bowling/kata/BowlingGameTest.java
+++ src/test/java/bowling/kata/BowlingGameTest.java
@@ -14,4 +14,11 @@ public class BowlingGameTest extends TestCase{
 		assertEquals(0, g.score());
 	}
 	
+	public void testAllOnes() throws Exception {
+		Game g = new Game();
+		for(int i = 0; i < 20; i++) {
+			g.roll(0);
+		}
+		assertEquals(20, g.score());
+	}
 }
